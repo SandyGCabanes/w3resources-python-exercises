a1046ae604a893e60d4d9556ca901d43d1a312a0
SandyGCabanes <sandygcabanes@gmail.com>
2019-04-10 12:10:56 +0800

    Add files via upload

---
+++ mini project 4_99bottles.py
@@ -0,0 +1,14 @@
+#sandygcabanes
+#practicepython.org
+#mini project 4 99 bottles
+''' Create a Python project that prints out every line of the song
+"99 bottles of beer on the wall." Note: Try to use a built in function instead
+of manually typing all the lines'''
+
+i = 99
+while i < 100 and i > 1:
+    print ('{} bottles of beer on the wall \n{} bottles of beer'.format(i,i))
+    print ('Take one down, pass it around')
+    i -= 1
+    print ('{} bottles of beer on the wall'.format (i))
+print ('One bottle of beer on the wall \nOne bottle of beer \nTake it down, pass it around\nNo more bottles of beer on the wall')
